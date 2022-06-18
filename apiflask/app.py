cb64a369d50a52186359440f50698f447535231a
fhernando13 <fhesr667@gmail.com>
2022-06-18 14:06:20 -0500

    api terminada

--- apiflask/app.py
+++ apiflask/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
-from flask_sqlachemy import SQLAlchemy
+from crypt import methods
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQL_ALCHEMY_DATABASE_URI']='mysql+pymsql://root@localhost/flaskmysql'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://user:password@localhost/flaskmysql'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db = SQLAlchemy(app)
@@ -32,9 +34,54 @@ tareas_schema = TareaSchema(many=True)
 @app.route('/tareas', methods=['POST'])
 def crear_tarea():
     
-    print(request.json)
-    return 'recibido'
+    titulo =  request.json['titulo']
+    descripcion = request.json['descripcion']
 
+    new_tarea = Tarea(titulo, descripcion)
+    db.session.add(new_tarea)
+    db.session.commit()
+
+    return tarea_schema.jsonify(new_tarea)
+
+@app.route('/leer_tareas', methods=['GET'])
+def leer_tareas():
+    
+    all_tareas = Tarea.query.all()
+    resultado = tareas_schema.dump(all_tareas)
+    return jsonify(resultado)
+
+@app.route('/leer_tarea_unica/<id>', methods=['GET'])
+def leer_tarea_unica(id):
+
+    tarea = Tarea.query.get(id)
+    return tarea_schema.jsonify(tarea)
+
+@app.route('/actualizar_tarea/<id>', methods=['PUT'])
+def actualizar_tarea(id):
+
+    tarea = Tarea.query.get(id)
+    titulo =  request.json['titulo']
+    descripcion = request.json['descripcion']
+
+    tarea.titulo = titulo
+    tarea.descripcion = descripcion
+
+    db.session.commit()
+
+    return tarea_schema.jsonify(tarea)
+
+@app.route('/eliminar_tarea/<id>', methods=['DELETE'])
+def eliminar_tarea(id):
+
+    tarea = Tarea.query.get(id)
+    db.session.delete(tarea)
+    db.session.commit()
+
+    return tarea_schema.jsonify(tarea)
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message':'Bienvenido a mi API'})
 
 if __name__ == '__main__':
     app.run(debug=True)
